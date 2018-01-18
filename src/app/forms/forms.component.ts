5df740aaefc1cc51e4d266fcc41f5ab805e53836
Aakashdeveloper <aakash.h@hcl.com>
2018-01-18 21:38:56 +0530

    A

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,10 +1,33 @@
-import {Component} from '@angular/core'
+import {Component} from '@angular/core';
+import {Employee} from '../models/employee.model';
 
 @Component({
     selector:'ang-forms',
-    templateUrl:'./form.component.html'
+    templateUrl:'./forms.component.html'
 })
 
 export class FormComponent{
+    languages=["Nodejs","AngularJS","ReactJS"]
+    model = new Employee("Edureka","Angular",0,true,"male","Nodejs")
+    hasCodeError=false
 
+    firstToUpper(value:string){
+        if(value.length>0)
+            this.model.firstname = value.charAt(0).toUpperCase() + value.slice(1)
+        else
+            this.model.firstname=value
+    }
+    validateLang(event){
+        if(this.model.codelang == "default")
+            this.hasCodeError=true
+        else
+            this.hasCodeError=false
+    }
 }
+
+/*
+ng-untouched  ng-pristine   ng-valid
+ng-touched    ng-dirty      ng-invalid
+(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)
+
+*/
---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,10 @@
+export class Employee{
+    constructor(
+        public firstname:string,
+        public lastname:string,
+        public phone:number,
+        public fullTime:boolean,
+        public gender:string,
+        public codelang:string,
+    ){}
+}
